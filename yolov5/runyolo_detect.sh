b30ad06959307f4e7caa1160e03ba4d160eed848
Dylee-y <dlekdls0121@gmail.com>
2023-06-14 23:34:59 +0900

    added extra dataset

--- yolov5/runyolo_detect.sh
+++ yolov5/runyolo_detect.sh
@@ -1,3 +1,3 @@
 python detect.py \
-  --weights /home/percv-d0/dyn/2023.1_BSpaper/yolov5/runs/train/exp22/weights/best.pt \
-  --source /home/percv-d0/dyn/2023.1_BSpaper/datasets/klicense_dataset/realimg
+  --weights /home/percv-d0/dyn/2023.1_BSpaper/yolov5/runs/train/exp23/weights/best.pt \
+  --source /home/percv-d0/dyn/2023.1_BSpaper/datasets/klicense_dataset/KakaoTalk_20230614_184807729.mp4
